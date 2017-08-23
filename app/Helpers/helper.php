1c944d7aea3d5e9844ba007ba7859ba8a63d1ca4
Abid Raza <ari.abid@gmail.com>
2017-08-23 16:58:32 +0530

    Change function name

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,5 +1,17 @@
 <?php
+
 function app_name()
 {
 	return config('app.name', 'Laravel Simple CMS');
+}
+
+function dashboardRoute()
+{
+	if( ! \Auth::check())
+		return null;
+
+	if(\Auth::user()->isAdmin)
+		return route('admin.dashboard');
+
+	return route('user.dashboard');
 }
